7a8f77de0e83c06ee175791d94666ce8696537ec
Satish Birajdar <satishbirajdar1@gmail.com>
2014-08-13 00:45:14 -0400

    Added new again.

---
+++ project/home.aspx.cs
@@ -0,0 +1,51 @@
+﻿/*
+ Name - Satish Birajdar
+ Description- This File will image buttons for putting a new book or add a book for sale.
+ * */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Data;
+
+
+namespace project
+{
+    public partial class home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            /*create a session for security and timeout of 5 mins*/
+            Session.Timeout = 5;
+            if (Session["UserName"] != null)
+            {
+                LblUsername.Text += Session["EmailID"].ToString();
+            }
+            else
+            {
+                Response.Redirect("index.aspx");
+            }
+
+        }
+
+
+        protected void putBtn_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("sell.aspx");
+        }
+
+        protected void buySale_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("buy.aspx");
+        }
+
+        protected void Unnamed_Click(object sender, ImageClickEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ project/registration.aspx.cs
@@ -0,0 +1,63 @@
+﻿/*
+ Name - Satish Birajdar
+ Description- This File will be used for creating new distinct user.
+ * */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+//add a reference to the sql client library so we can connect
+using System.Data.SqlClient;
+
+//add a reference so we can read settings from web.config
+using System.Configuration;
+
+namespace project
+{
+    public partial class registration : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Btn_Submit_Click(object sender, EventArgs e)
+        {
+            using (BookSaleEntities Entities = new BookSaleEntities())
+            {
+                /*check database whether the user already exists*/
+                if (Entities.Users.FirstOrDefault(u => u.username.ToLower() == TxtUserName.Text.ToLower()) != null)
+                {
+                    pnlUserNameError.Visible = true;
+                }
+                else
+                {
+                    User user = new User();
+
+                    user.username = TxtUserName.Text;
+                    user.password = PasswordHash.CreateHash(TxtPassword.Text);
+                    user.email_id = TxtEmailId.Text;
+                    user.phone_number = TxtPhoneNumber.Text;
+                    Entities.Users.Add(user);
+
+                    Entities.SaveChanges();
+
+                    Response.Redirect("index.aspx");
+
+                }
+            }
+
+        }
+
+
+        protected void Btn_Cancel_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("index.aspx");
+        }
+    }
+}
